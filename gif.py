4914aa39214639129639b44c266c86292d1393fe
seyyed mohammad mahdi razzaghi <razzaghimohammadmahdi@gmail.com>
2024-03-15 23:22:06 +0330

    update

--- gif.py
+++ gif.py
@@ -1,12 +1,12 @@
 import os
 import imageio
 
-file_list = sorted(os.listdir("Assignment-8\img"))
+file_list = sorted(os.listdir("img"))
 
 IMAGES = []
 for file_name in file_list:
-    file_path = "Assignment-8\img" + file_name
+    file_path = "img/" + file_name
     image = imageio.imread(file_path)
     IMAGES.append(image)
 
-imageio.mimsave("Assignment-8/my_output.gif", IMAGES)
+imageio.mimsave("my_output.gif", IMAGES)
